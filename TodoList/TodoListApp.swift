cfb94f7acaba124aebb327edbe46c471b5f1d8a3
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-07-22 13:44:58 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Boris R on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
